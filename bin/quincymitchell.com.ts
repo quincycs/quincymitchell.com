9b0354616ee16ef8c68201ccdb9b951535f16d93
quincycs <2924643+quincycs@users.noreply.github.com>
2022-03-29 22:21:11 -0400

    init

--- bin/quincymitchell.com.ts
+++ bin/quincymitchell.com.ts
@@ -5,6 +5,7 @@ import { QuincymitchellComStack } from '../lib/quincymitchell.com-stack';
 
 const app = new cdk.App();
 new QuincymitchellComStack(app, 'QuincymitchellComStack', {
+  stackName: 'prod-quincymitchell-com'
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- lib/quincymitchell.com-stack.ts
+++ lib/quincymitchell.com-stack.ts
@@ -1,16 +1,45 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
+import { AllowedMethods, CachedMethods, Distribution, OriginRequestPolicy, ResponseHeadersPolicy, ViewerProtocolPolicy } from 'aws-cdk-lib/aws-cloudfront';
+import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import { BlockPublicAccess, Bucket, HttpMethods } from 'aws-cdk-lib/aws-s3';
+import { StringParameter } from 'aws-cdk-lib/aws-ssm';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+const domainName = 'quincymitchell.com';
+const ssmCertArn = '/prod/quincymitchell.com/acm/certArn';
 
 export class QuincymitchellComStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const certArnParam = StringParameter.fromStringParameterName(this, 'ssmCertArn', ssmCertArn);
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'QuincymitchellComQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const myBucket = new Bucket(this, 'bucket', {
+      bucketName: `prod-${domainName}`,
+      cors: [{
+        allowedMethods: [HttpMethods.GET],
+        allowedOrigins: ['*'],
+        allowedHeaders: ['*']
+      }],
+      enforceSSL: true,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: RemovalPolicy.RETAIN
+    });
+    const mycert = Certificate.fromCertificateArn(this, 'certificate', certArnParam.stringValue);
+
+    new Distribution(this, 'myDist', {
+      defaultBehavior: {
+        origin: new S3Origin(myBucket),
+        viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        originRequestPolicy: OriginRequestPolicy.CORS_S3_ORIGIN,
+        responseHeadersPolicy: ResponseHeadersPolicy.CORS_ALLOW_ALL_ORIGINS,
+        allowedMethods: AllowedMethods.ALLOW_GET_HEAD_OPTIONS, // needed for cors
+        cachedMethods: CachedMethods.CACHE_GET_HEAD_OPTIONS, // needed for cors
+      },
+      defaultRootObject: 'index.html',
+      domainNames: [domainName, `www.${domainName}`],
+      certificate: mycert
+    });
   }
 }
